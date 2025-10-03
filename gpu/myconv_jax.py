383034ca13e9b7e68bb210996ab9c468d933d053
Aldo Zhang <aldozhang1@gmail.com>
2025-10-02 23:30:22 -0500

    jax implementation:

--- gpu/myconv_jax.py
+++ gpu/myconv_jax.py
@@ -24,8 +24,15 @@ def im2col_manual_jax(x, KH, KW, S, P, out_h, out_w):
     # TO DO: Convert input (x) into shape (N, out_h*out_w, C*KH*KW). 
     # Refer to Lecture 3 for implementing this operation.
     
-    # patches = ...
-    # return patches
+    patches_list = []
+    for i in range(out_h):
+        for j in range(out_w):
+            sh, sw = i * S, j * S
+            window = x_pad[:, :, sh:sh + KH, sw:sw + KW]  # (N, C, KH, KW)
+            patches_list.append(window.reshape(N, C * KH * KW))  # (N, CK2)
+
+    patches = jnp.stack(patches_list, axis=1)  # (N, out_h*out_w, C*KH*KW)
+    return patches
 
 def conv2d_manual_jax(x, weight, bias, stride=1, padding=1):
     '''
@@ -37,21 +44,41 @@ def conv2d_manual_jax(x, weight, bias, stride=1, padding=1):
     C_out, _, KH, KW = weight.shape
 
     # define your helper variables here
-    # out_h = ...
-    # out_w = ...
+    S, P = stride, padding
+    out_h = (H + 2 * P - KH) // S + 1
+    out_w = (W + 2 * P - KW) // S + 1
     
     # TO DO: 1) convert input (x) into shape (N, out_h*out_w, C*KH*KW).
-    # cols = im2col_manual_jax(x, KH, KW, stride, padding, out_h, out_w)
-
+    cols = im2col_manual_jax(x, KH, KW, stride, padding, out_h, out_w)
+    K = C * KH * KW
+    R = out_h * out_w
     # TO DO: 2) flatten self.weight into shape (C_out, C*KH*KW).
+    flat_weight = weight.reshape(C_out, K)
 
     # TO DO: 3) perform tiled matmul after required reshaping is done.
+    cols_reshaped = cols.reshape(N * R, K)                            
+    out_flat = jnp.zeros((N * R, C_out), dtype=cols_reshaped.dtype)
+
+    RB = 2048   # row tile
+    CB = 256    # col tile
+
+    for r0 in range(0, N * R, RB):
+        r1 = min(r0 + RB, N * R)
+        A = cols_reshaped[r0:r1, :]                                   
+        for c0 in range(0, C_out, CB):
+            c1 = min(c0 + CB, C_out)
+            B = flat_weight[c0:c1, :].T                               
+            tile = A @ B                                             
+            out_flat = out_flat.at[r0:r1, c0:c1].set(tile)
 
     # TO DO: 4) Add bias.
+    out_flat = out_flat + bias                                        
 
     # TO DO: 5) reshape output into shape (N, C_out, out_h, out_w).
-
-    #return out
+    out = out_flat.reshape(N, R, C_out)                          
+    out = jnp.transpose(out, (0, 2, 1))                              
+    out = out.reshape(N, C_out, out_h, out_w)                         
+    return out
 
 if __name__ == "__main__":
     # Instantiate PyTorch model
